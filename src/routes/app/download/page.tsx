c3c7f48b078855a9a90e21745d2a5b8c4604edfc
CodePwn2021 <77036672+CodePwn2021@users.noreply.github.com>
2024-10-04 00:43:53 +0800

    fix install script, add buttons for copy contents to clipboard

--- src/routes/app/download/page.tsx
+++ src/routes/app/download/page.tsx
@@ -1,5 +1,15 @@
-import { Anchor, ScrollArea, Stack, Text, Title } from '@mantine/core';
+import {
+  Anchor,
+  Button,
+  Group,
+  ScrollArea,
+  Stack,
+  Text,
+  Title,
+} from '@mantine/core';
 import { CodeHighlight } from '@mantine/code-highlight';
+import { notifications } from '@mantine/notifications';
+import CopyToClipboard from 'copy-to-clipboard';
 import PageTitle from '@/ui/component/app/PageTitle';
 import MV4Card from '@/ui/component/app/MV4Card';
 
@@ -13,7 +23,14 @@ make current
     'export PB_USE_GH_REPO=1 && export GH_DOMAIN="https://github.com/" && bash -c "$(curl -fsSL raw.githubusercontent.com/bouldev/PhoenixBuilder/main/install.sh)"';
 
   const downloadWithProxyShellCode =
-    'export PB_USE_GH_REPO=1 && export GH_DOMAIN="https://ghp.ci/" && bash -c "$(curl -fsSL ghp.ci/bouldev/PhoenixBuilder/main/install.sh)"';
+    'export PB_USE_GH_REPO=1 && export GH_DOMAIN="https://ghp.ci/https://github.com/" && bash -c "$(curl -fsSL https://ghp.ci/https://raw.githubusercontent.com/bouldev/PhoenixBuilder/main/install.sh)"';
+
+  function onClickCopy(str: string) {
+    CopyToClipboard(str);
+    notifications.show({
+      message: '已将内容复制到剪贴板',
+    });
+  }
 
   return (
     <Stack>
@@ -31,14 +48,22 @@ make current
           </Stack>
           <Text size="sm">
             若您访问 GitHub
-            存在一定困难/使用上面的安装脚本出现报错，您可以尝试下方使用了 ghp.ci
-            的安装脚本。
+            存在一定困难/使用上面的安装脚本出现报错，您可以尝试下方使用 ghp.ci
+            进行代理加速的安装脚本。
           </Text>
           <Stack gap={'xs'}>
             <ScrollArea>
               <CodeHighlight code={downloadWithProxyShellCode} lang={'sh'} />
             </ScrollArea>
           </Stack>
+          <Group gap={'sm'}>
+            <Button onClick={() => onClickCopy(downloadShellCode)}>
+              复制脚本
+            </Button>
+            <Button onClick={() => onClickCopy(downloadWithProxyShellCode)}>
+              复制脚本 (代理加速)
+            </Button>
+          </Group>
         </Stack>
       </MV4Card>
       <MV4Card>
@@ -49,6 +74,11 @@ make current
               <CodeHighlight code={buildShellCode} lang={'sh'} />
             </ScrollArea>
           </Stack>
+          <Group gap={'sm'}>
+            <Button onClick={() => onClickCopy(buildShellCode)}>
+              复制代码
+            </Button>
+          </Group>
         </Stack>
       </MV4Card>
       <MV4Card>
